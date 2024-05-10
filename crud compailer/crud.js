9fdfe48cd227c1d1b1416158cefde2ee6b6c7806
jenil gajera <gajerajenil08@gmail.com>
2024-05-10 17:28:09 +0530

    crud is 80% complate

--- crud compailer/crud.js
+++ crud compailer/crud.js
@@ -14,7 +14,7 @@ let PlayingCricet = document.getElementById("Playing-cricet")
 
 let dropdown = document.getElementById("city");
 let value = []
-
+       
 // const dropdown = document.getElementById("city");
 const selectedValue = dropdown.value;
 console.log(selectedValue);
@@ -44,18 +44,6 @@ const save = () => {
     console.log(value)
 
     const tableBody = document.getElementById('table-body');
-
-    const tableData = document.getElementsByClassName('table-data');
-
-    // for (let i = 0; i < tableData.length; i++) {
-    //     document.getElementById('id').innerText = value[i].id
-    //     document.getElementById('name').innerText = value[i].name
-    //     document.getElementById('email').innerText = value[i].email
-    //     document.getElementById('password').innerText = value[i].password
-    //     document.getElementById('number').innerText = value[i].Number
-    //     document.getElementById('gender').innerText = value[i].gender
-    //     document.getElementById('hobby').innerText = value[i].interests
-    // }
 
     for (let i = 0; i < value.length; i++) {
         const tr = document.createElement('tr');
