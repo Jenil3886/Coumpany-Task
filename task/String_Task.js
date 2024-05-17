41edc383b6303f127d02afd72271e47f3bf2a9e0
Jenil3886 <gajerajenil08@gmail.com>
2024-05-17 09:53:44 +0530

    calculator in  80%  complate

--- task/String_Task.js
+++ task/String_Task.js
@@ -12,49 +12,49 @@ console.log(capitalizedWords);
 // 2. check string pelidrom or not 
 
 
-function isPalindrome(str) {
-    let j = str.length - 1
-    for (let i = 0; i < str.length / 2; i++) {
-        if (str[i] != str[j]) {
-            return "it is not a palindron";
-        }
-        j--;
-    }
-    return "it is palindron";
-}
+// function isPalindrome(str) {
+//     let j = str.length - 1
+//     for (let i = 0; i < str.length / 2; i++) {
+//         if (str[i] != str[j]) {
+//             return "it is not a palindron";
+//         }
+//         j--;
+//     }
+//     return "it is palindron";
+// }
 
-let str1 = "naman a naman";
-let str2 = "lorem epsum dolor";
+// let str1 = "naman a naman";
+// let str2 = "lorem epsum dolor";
 
-console.log(isPalindrome(str1));
-console.log(isPalindrome(str2));
+// console.log(isPalindrome(str1));
+// console.log(isPalindrome(str2));
 
 
 // 3. worde is asending order with capital
 
-let str = "ipsum dolor sit amet";
+// let str = "ipsum dolor sit amet";
 
-let words = str.split(" ");
+// let words = str.split(" ");
 
-words.sort();
+// words.sort();
 
-let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+// let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
 
-let result = capitalizedWords.join(" ");
+// let result = capitalizedWords.join(" ");
 
-console.log(result);
+// console.log(result);
 
 // 4. 'a' carector are repeat and how many time
 
 
-let str = 'motavarachasurat';
-let count = 0;
+// let str = 'motavarachasurat';
+// let count = 0;
 
-for (let i = 0; i < str.length; i++) {
-    if (str[i] === 'a') {
-        count++;
-    }
-}
+// for (let i = 0; i < str.length; i++) {
+//     if (str[i] === 'a') {
+//         count++;
+//     }
+// }
 
-console.log("The character 'a' is repeated", count, "times.");
+// console.log("The character 'a' is repeated", count, "times.");
 
