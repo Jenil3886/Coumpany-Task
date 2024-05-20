b33427e7b2fbd76aed29a1bce26fb9f7a0e8155b
Jenil3886 <gajerajenil08@gmail.com>
2024-05-20 17:49:09 +0530

    calculator are complate

--- JavaScript/Calculator/index.js
+++ JavaScript/Calculator/index.js
@@ -1,65 +1,13 @@
-// // Function that display value
-// function dis(val) {
-//   document.getElementById("result").value += val;
-// }
-
-// function myFunction(event) {
-//   const display = document.getElementById("result");
-//   if (
-//     display?.value !== "0" &&
-//     (event.key == "0" ||
-//       event.key == "1" ||
-//       event.key == "2" ||
-//       event.key == "3" ||
-//       event.key == "4" ||
-//       event.key == "5" ||
-//       event.key == "6" ||
-//       event.key == "7" ||
-//       event.key == "8" ||
-//       event.key == "9" ||
-//       event.key == "+" ||
-//       event.key == "-" ||
-//       event.key == "*" ||
-//       event.key == "/")
-//   )
-//     document.getElementById("result").value += event.key;
-// }
-
-// var cal = document.getElementById("calcu");
-// cal.onkeyup = function (event) {
-//   if (event.keyCode === 13) {
-//     console.log("Enter");
-//     let x = document.getElementById("result").value;
-//     console.log(x);
-//     solve();
-//   }
-// };
-
-// // Function that evaluates the digit and return result
-// function solve() {
-//   let x = document.getElementById("result").value;
-
-//   let y = eval(x);
-//   document.getElementById("result").value = y;
-// }
-
-// // Function that clear the display
-// function clr() {
-//   document.getElementById("result").value = "0";
-// }
-
-// function del() {
-//   var value = document.getElementById("result").value;
-//   document.getElementById("result").value =
-//     value.length === 1 ? 0 : value.substring(0, value.length - 1);
-// }
-
-
-
 let display = document.getElementById('display');
 
+display.value = '0';
+
 function appendToDisplay(value) {
-  display.value += value;
+  if (display.value === '0' && (value === '.' || !isNaN(value))) {
+    display.value = value;
+  } else {
+    display.value += value;
+  }
 }
 
 function clearDisplay() {
@@ -76,7 +24,7 @@ function calculate() {
 
 document.addEventListener('keydown', function(event) {
   const key = event.key;
-  const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','Enter','Backspace','Escape'];
+  const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','.','+','-','*','/','%','Enter','Backspace','Escape'];
 
   if (allowedKeys.includes(key)) {
     event.preventDefault();
