6a82473b5164984bcf10159067db0ef94b700f55
Jenil Gajera <146574715+Jenil3886@users.noreply.github.com>
2024-05-28 11:24:18 +0530

    string task complate in company task

--- JavaScript/task/String_Task.js
+++ JavaScript/task/String_Task.js
@@ -6,25 +6,27 @@ let words = str.split(" ");
 
 let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
 
-console.log(capitalizedWords);
+// console.log(capitalizedWords);
 
 
-// 2. check string pelidrom or not 
+// 2. check string pelindrom or not 
 
 
-// function isPalindrome(str) {
-//     let j = str.length - 1
-//     for (let i = 0; i < str.length / 2; i++) {
-//         if (str[i] != str[j]) {
-//             return "it is not a palindron";
-//         }
-//         j--;
-//     }
-//     return "it is palindron";
-// }
+ function isPalindrome(str) {
+    let j = str.length - 1
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[j]) {
+            return "it is not a palindron";
+        }
+        j--;
+     }
 
-// let str1 = "naman a naman";
-// let str2 = "lorem epsum dolor";
+    return "it is palindron";
+
+ }
+
+let str1 = "naman a naman";
+let str2 = "lorem epsum dolor";
 
 // console.log(isPalindrome(str1));
 // console.log(isPalindrome(str2));
@@ -32,29 +34,31 @@ console.log(capitalizedWords);
 
 // 3. worde is asending order with capital
 
-// let str = "ipsum dolor sit amet";
+let string = "ipsum dolor sit amet";
 
-// let words = str.split(" ");
+let word = str.split(" ");
 
-// words.sort();
+words.sort();
 
-// let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+let capitalizedWord = word.map(word => word.charAt(0).toUpperCase() + word.slice(1));
 
-// let result = capitalizedWords.join(" ");
+
+let letters = [...capitalizedWord]
+
+let result = letters.join(" ");
 
 // console.log(result);
 
 // 4. 'a' carector are repeat and how many time
 
 
-// let str = 'motavarachasurat';
-// let count = 0;
+let stri = 'motavarachasurat';
+let count = 0;
 
-// for (let i = 0; i < str.length; i++) {
-//     if (str[i] === 'a') {
-//         count++;
-//     }
-// }
-
-// console.log("The character 'a' is repeated", count, "times.");
+for (let i = 0; i < stri.length; i++) {
+   if (stri[i] === 'a') {
+        count++;
+    }
+ }
+ console.log("The character 'a' is repeated", count, "times.");
 
--- prictic/tic tac toe/app.js
+++ prictic/tic tac toe/app.js
@@ -4,17 +4,17 @@ let msgContainer = document.querySelector(".msg-container")
 let msg = document.querySelector("#msg")
 let newGameBtn = document.querySelector("#new-btn")
 
-let turnO = true;  // playerX , palyerO                                                               // stap 1
+let turnO = true;  // playerX , palyerO                                                          
    
 // 2D array
 const winPatterns = [
-    [0, 1, 2],                                                                                        // 
-    [0, 3, 6],                                                                                        //
-    [0, 4, 8],                                                                                        // Stap 2
-    [1, 4, 7],                                                                                        //
-    [2, 5, 8],                                                                                        //
-    [2, 4, 6],                                                                                        //
-    [3, 4, 5],                                                                                        // 
+    [0, 1, 2],                                                                                
+    [0, 3, 6],                                                                              
+    [0, 4, 8],                                                                                       
+    [1, 4, 7],                                                                                
+    [2, 5, 8],                                                                                        
+    [2, 4, 6],                                                                                        
+    [3, 4, 5],                                                                                         
     [6, 7, 8],       
 ];
 
