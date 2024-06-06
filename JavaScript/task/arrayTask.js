41de3c1b9704c8a6e2b46e99e8da6e5e3226b58a
Jenil Gajera <146574715+Jenil3886@users.noreply.github.com>
2024-06-06 23:24:32 +0530

    first arr task is complate

--- JavaScript/task/arrayTask.js
+++ JavaScript/task/arrayTask.js
@@ -1,3 +1,7 @@
 // 1. Get all the number from array 
 
 let arr = [1, 3, "Test", 0, "", -1, null, undefined, "Hello World", "hello", "workd", 54, false, true, "-0", "null"];
+
+let number = arr.filter(element => typeof element === 'number')
+
+console.log(number)
