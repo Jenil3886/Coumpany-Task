41194fd45d183a1adae58cd424b07d725fb54016
Jenil Gajera <146574715+Jenil3886@users.noreply.github.com>
2024-06-06 23:27:06 +0530

    array second task is complate

--- JavaScript/task/arrayTask.js
+++ JavaScript/task/arrayTask.js
@@ -5,3 +5,9 @@ let arr = [1, 3, "Test", 0, "", -1, null, undefined, "Hello World", "hello", "wo
 let number = arr.filter(element => typeof element === 'number')
 
 console.log(number)
+
+// 2. Get all the string from array
+
+let string = arr.filter(element => typeof element === 'string')
+
+console.log(string)
